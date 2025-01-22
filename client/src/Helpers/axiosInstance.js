7ccf201d02f3f60364716e381bc883e151b4ec33
Akshat Jain <akshatjain4545@gmail.com>
2025-01-22 12:04:59 +0530

    base url corrected

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://damac-server.onrender.com";
+const BASE_URL = "https://damac-server.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
